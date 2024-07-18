3a16c0bbc36a622735b784572deb8ec9844d380f
ToSads <toSads057@gmail.com>
2024-07-18 22:49:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function examClick(examId) {
         console.error('error: more than exam with this id')
     } else {
         localStorage.setItem('exam', JSON.stringify({"exams": exam[0]}))
-        parent.location.pathname = 'tosadsTest/examPage.html'
+        parent.location.pathname = '/tosadsTest/examPage.html'
 
     }
 }
